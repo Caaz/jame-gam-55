4d73fc3da03a99f63878039aed061480fadfe3fc
Daniel Cavazos <daniel@caaz.me>
2025-11-20 08:27:17 -0800

    Simplify tileset.

--- scenes/tileset/tileset.gd
+++ scenes/tileset/tileset.gd
@@ -15,28 +15,26 @@ func _remove_hitbox_meshes() -> void:
 				mesh.queue_free()
 
 func _create_hitboxes() -> void:
-	for child:Node in get_children():
-		for mesh:MeshInstance3D in child.get_children():
-			var base_mesh_name:String = mesh.name.lstrip(child.name)
-			var name_parts:PackedStringArray = base_mesh_name.split("_")
-			var mesh_name:String = name_parts[0]
-			var mesh_index:String = ("_%s" % name_parts[1]) if name_parts.size() > 1 else ""
-			await _remove_children(mesh)
-			if hitbox_as_is.has(mesh_name):
-				mesh.create_trimesh_collision()
-			else:
-				var hitbox_name:String = "%s%sHitbox%s" % [child.name, mesh_name, mesh_index]
-				var hitbox_mesh:MeshInstance3D = child.find_child(hitbox_name)
-				if not hitbox_mesh:
-					continue
-				var shape:ConcavePolygonShape3D = hitbox_mesh.mesh.create_trimesh_shape()
-				var collision_shape:CollisionShape3D = CollisionShape3D.new()
-				collision_shape.shape = shape
-				var hitbox:StaticBody3D = StaticBody3D.new()
-				hitbox.add_child(collision_shape)
-				mesh.add_child(hitbox)
-				hitbox.owner = get_tree().edited_scene_root
-				collision_shape.owner = get_tree().edited_scene_root
+	for mesh:MeshInstance3D in get_children():
+		var name_parts:PackedStringArray = mesh.name.split("_")
+		var mesh_name:String = name_parts[0]
+		var mesh_index:String = ("_%s" % name_parts[1]) if name_parts.size() > 1 else ""
+		await _remove_children(mesh)
+		if hitbox_as_is.has(mesh_name):
+			mesh.create_trimesh_collision()
+		else:
+			var hitbox_name:String = "%sHitbox%s" % [mesh_name, mesh_index]
+			var hitbox_mesh:MeshInstance3D = find_child(hitbox_name)
+			if not hitbox_mesh:
+				continue
+			var shape:ConcavePolygonShape3D = hitbox_mesh.mesh.create_trimesh_shape()
+			var collision_shape:CollisionShape3D = CollisionShape3D.new()
+			collision_shape.shape = shape
+			var hitbox:StaticBody3D = StaticBody3D.new()
+			hitbox.add_child(collision_shape)
+			mesh.add_child(hitbox)
+			hitbox.owner = get_tree().edited_scene_root
+			collision_shape.owner = get_tree().edited_scene_root
 
 func _remove_children(node:Node) -> void:
 	for child:Node in node.get_children():
